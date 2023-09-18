54221ec38c29899c6447c7cbf1d205689e0da255
ODaviOTal <davialveskd4306@gmail.com>
2023-09-18 20:12:48 -0300

    wip

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 
 const homeRoutes = require('./routes/rotasHome'); 
 const admRoutes = require('./routes/rotasAdm'); 
+const loginRoutes = require('./routes/rotasLogin');
 
 app.use('/home', express.static('home.html'));
 app.use('/public', express.static('public')); 
 
 app.use('/', homeRoutes);
 app.use('/adm', admRoutes);
+app.use('/login', loginRoutes )
 
 app.listen(3000, function() {
     console.log("http://localhost:3000");
